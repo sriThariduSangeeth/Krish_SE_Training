f72f56d7b053e9dac1938f5ac46dbe5244b8d73e
sriThariduSangeeth <dtsangeeth@gmail.com>
2020-12-17 03:03:01 +0530

    refactor code base and create classes

---
+++ Lp-capitalization-string/src/com/virtusa/iofile/UpperCaseReader.java
@@ -0,0 +1,9 @@
+package com.virtusa.iofile;
+
+/**
+ * @author dtsangeeth
+ * @created 17 / 12 / 2020
+ * @project LPTraining
+ */
+public class UpperCaseReader {
+}
---
+++ Lp-capitalization-string/src/com/virtusa/main/Main.java
@@ -0,0 +1,13 @@
+package com.virtusa.main;
+
+/**
+ * @author dtsangeeth
+ * @created 17 / 12 / 2020
+ * @project LPTraining
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
