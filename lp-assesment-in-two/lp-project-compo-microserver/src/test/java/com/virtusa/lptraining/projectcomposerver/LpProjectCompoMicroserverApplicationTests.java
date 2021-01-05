b796e981d1333532d2fb13d775c2d2b0f217da91
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-01-05 21:19:26 +0530

    change gitignore 2

---
+++ lp-assesment-in-two/lp-project-compo-microserver/src/test/java/com/virtusa/lptraining/projectcomposerver/LpProjectCompoMicroserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.lptraining.projectcomposerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpProjectCompoMicroserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
