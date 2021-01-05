0e769f33dbceb31f2f9c0cdfadb1fb83e0fcac19
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-01-05 16:27:22 +0530

    task server create

---
+++ lp-assesment-in-two/lp-task-compo-microserver/src/main/java/com/virtusa/lptraining/taskserver/main/Application.java
@@ -0,0 +1,9 @@
+package com.virtusa.lptraining.taskserver.main;
+
+/**
+ * @author dtsangeeth
+ * @created 05 / 01 / 2021
+ * @project lp-task-compo-microserver
+ */
+public class Application {
+}
---
+++ lp-assesment-in-two/lp-task-compo-microserver/src/main/java/com/virtusa/lptraining/taskserver/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.virtusa.lptraining.taskserver.utils;
+
+/**
+ * @author dtsangeeth
+ * @created 05 / 01 / 2021
+ * @project lp-task-compo-microserver
+ */
+public class Constants {
+}
