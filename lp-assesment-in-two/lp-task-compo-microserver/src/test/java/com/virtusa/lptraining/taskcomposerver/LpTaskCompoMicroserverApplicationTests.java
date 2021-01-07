059999303d6416db331bca5af1c7d9351ca3050a
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-01-08 01:42:40 +0530

    create registy yml files

---
+++ lp-assesment-in-two/lp-task-compo-microserver/src/test/java/com/virtusa/lptraining/taskcomposerver/LpTaskCompoMicroserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.lptraining.taskcomposerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpTaskCompoMicroserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
